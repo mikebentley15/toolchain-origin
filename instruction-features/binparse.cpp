db472b51c2f96b5abec6753296005c3e87db89e0
Michael Bentley <mikebentley15@gmail.com>
2017-04-21 21:28:10 -0600

    instr-parse: Add some printing for each function

--- instruction-features/binparse.cpp
+++ instruction-features/binparse.cpp
@@ -2,6 +2,7 @@
 
 #include <CodeObject.h>
 #include <CodeSource.h>
+#include <CFG.h>
 
 #include <memory>
 
@@ -22,12 +23,22 @@ void parse_binary(const string &infile, ostream &out, Dyninst::Address start) {
       << "Would have read in " << infile << endl;
   Dyninst::ParseAPI::SymtabCodeSource source(infile_copy.get());
   Dyninst::ParseAPI::CodeObject obj(&source);
-  if (start == 0)
+  if (start == 0) {
     obj.parse();
-  else
-    obj.parse(start, true);
+  } else {
+    obj.parse(start, false); // true);
+  }
 
-  
+  auto funcs = obj.funcs();
+  for (auto func : funcs) {
+    out << func->name() << endl
+        << "  Address:   " << func->addr() << endl
+        << "  Is leaf:   " << func->_is_leaf_function << endl
+        << "  Ret addr:  " << func->_ret_addr << endl
+        << "  Is parsed: " << func->parsed() << endl
+        << "  # blocks:  " << func->num_blocks() << endl
+        << endl;
+  }
 
   // TODO: analyze what was parsed
 }
